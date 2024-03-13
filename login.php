2a1e4a1152244593ee1ec24f468ab3f58455f0b0
ctsJaspreetSingh <91944515+ctsJaspreetSingh@users.noreply.github.com>
2024-03-13 14:43:11 +0100

    Projekt aufgesetzt & Grundgerüst erstellt

---
+++ login.php
@@ -0,0 +1,53 @@
+<?php 
+session_start();
+
+$page_title = "Login Page";
+include('includes/header.php'); 
+include('includes/navbar.php'); 
+?>
+
+<div class="py-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+            <div class="alert">
+                    <?php
+                        if(isset($_SESSION['status']))
+                        {
+                            echo "<h4>".$_SESSION['status']."</h4>";
+                            unset($_SESSION['status']);
+                        }
+                    ?>
+                </div>
+
+                <div class="card shadow">
+                    <div class="card-header">
+                        <h5>Login Form</h5>
+                    </div>
+
+                    <div class="card-body">
+                        
+                        <form action="code.php" method="POST">
+                           
+                            <div class="form-group mb-3">
+                                <label for="">Email Address</label>
+                                <input type="text" name="email" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="">Password</label>
+                                <input type="password" name="password" class="form-control">
+                            </div>
+                          
+                            <div class="form-group">
+                                <button type="submit" name="register_btn" class="btn btn-primary">Login Now</button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
---
+++ register.php
@@ -0,0 +1,63 @@
+<?php 
+session_start();
+
+$page_title = "Registration Page";
+include('includes/header.php'); 
+include('includes/navbar.php'); 
+?>
+
+<div class="py-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+            <div class="alert">
+                    <?php
+                        if(isset($_SESSION['status']))
+                        {
+                            echo "<h4>".$_SESSION['status']."</h4>";
+                            unset($_SESSION['status']);
+                        }
+                    ?>
+                </div>
+
+                <div class="card shadow">
+                    <div class="card-header">
+                        <h5>Registration form</h5>
+                    </div>
+
+                    <div class="card-body">
+                        
+                        <form action="code.php" method="POST">
+                            <div class="form-group mb-3">
+                                <label for="">Name</label>
+                                <input type="text" name="name" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="">Phone Number</label>
+                                <input type="text" name="phone" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="">Email Address</label>
+                                <input type="text" name="email" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="">Password</label>
+                                <input type="password" name="password" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="">Confirm Password</label>
+                                <input type="password" name="confirm_password" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" name="register_btn" class="btn btn-primary">Register Now</button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
